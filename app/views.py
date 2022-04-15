e77576b40e21a494bfcc3d91168dbdf9fed94394
asmath <ameerbashad90@gmail.com>
2022-04-15 16:17:49 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from app.models import *
+# Create your views here.
+def display_topics(request):
+    topics=Topic.objects.all()
+    d={'ts':topics}
+    return render(request,'display_topics.html',d)
+
+def display_webpages(request):
+    webpages=Webpage.objects.all()
+    d={'ws':webpages}
+    return render(request,'display_webpages.html',d)
