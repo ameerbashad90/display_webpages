6d531bd5f092753fe4abed4cf3c27a5dd67b4bae
asmath <ameerbashad90@gmail.com>
2022-04-18 18:45:21 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -8,10 +8,24 @@ def display_topics(request):
 
 def display_webpages(request):
     webpages=Webpage.objects.all()
+    #webpages=Webpage.objects.filter(topic_name='FootBall')
+    #webpages=Webpage.objects.filter(topic_name='Cricket')
+    #webpages=Webpage.objects.filter(name='virat')
+    #webpages=Webpage.objects.filter(topic_name='kabaddi')
+    #webpages=Webpage.objects.filter(name__startswith='s')
+    #webpages=Webpage.objects.filter(name__endswith='s')
+    webpages=Webpage.objects.filter(url__startswith='https')
+
     d={'ws':webpages}
     return render(request,'display_webpages.html',d)
 
 def display_access(request):
     access=AccessRecords.objects.all()
+    access=AccessRecords.objects.filter(date='2020-06-26')
+    access=AccessRecords.objects.filter(date__month='03')
+    access=AccessRecords.objects.filter(date__month='09')
+    access=AccessRecords.objects.filter(date__year='2000')
+    access=AccessRecords.objects.filter(date__month__gt='03')
+    access=AccessRecords.objects.filter(date__day__gte='03')
     d={'ac':access}
     return render(request,'display_access.html',d)
